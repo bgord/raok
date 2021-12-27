ada0f6011f5f2a510b72292dfeeac3b47837e35f
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-12-27 23:32:19 +0100

    feature: display current number of articles to autosend

--- routes/dashboard.ts
+++ routes/dashboard.ts
@@ -1,13 +1,18 @@
 import express from "express";
 import { CsrfShield } from "@bgord/node";
 
+import { Settings } from "../aggregates/settings";
+
 export async function Dashboard(
   request: express.Request,
   response: express.Response,
   _next: express.NextFunction
 ): Promise<void> {
+  const settings = await new Settings().build();
+
   const vars = {
     username: request.user,
+    ...settings,
     ...CsrfShield.extract(request),
   };
 
