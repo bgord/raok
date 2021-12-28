0f7f161e75f6b34ba698ee9b35d49f29df8bdb8b
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-12-29 00:17:34 +0100

    feature: add create-newspaper form ui

--- static/main.js
+++ static/main.js
@@ -16,3 +16,29 @@ deselectAllButton.addEventListener("click", () => {
     node.checked = false;
   });
 });
+
+const createNewspaperForm = document.querySelector("#create-newspaper");
+
+createNewspaperForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const selectedArticleIds = Array.from(document.querySelectorAll("#urls"))
+    .filter((node) => node.checked)
+    .map((node) => node.name);
+
+  if (selectedArticleIds.length === 0) {
+    return;
+  }
+
+  const csrfToken = document.querySelector(
+    'form#create-newspaper > input[name="_csrf"]'
+  )?.value;
+
+  return fetch("/create-newspaper", {
+    method: "POST",
+    body: JSON.stringify({
+      _csrf: csrfToken,
+      articleIds: selectedArticleIds,
+    }),
+  });
+});
