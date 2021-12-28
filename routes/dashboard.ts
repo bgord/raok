6ce8a7e8af58e471f21843d885e8fbf118df4e49
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-12-28 16:46:20 +0100

    feature: display articles

--- routes/dashboard.ts
+++ routes/dashboard.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { CsrfShield } from "@bgord/node";
 
 import { Settings } from "../aggregates/settings";
+import { Articles } from "../aggregates/articles";
 
 export async function Dashboard(
   request: express.Request,
@@ -9,9 +10,11 @@ export async function Dashboard(
   _next: express.NextFunction
 ): Promise<void> {
   const settings = await new Settings().build();
+  const articles = (await new Articles().build()).articles;
 
   const vars = {
     username: request.user,
+    articles,
     ...settings,
     ...CsrfShield.extract(request),
   };
