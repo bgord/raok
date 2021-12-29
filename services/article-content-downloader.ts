608bde77a985f07f6eceea8d9dd94fa739cbcbb9
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-12-29 23:56:15 +0100

    step: add article content downloader service

---
+++ services/article-content-downloader.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+import * as VO from "../value-objects";
+
+export class ArticleContentDownloader {
+  static async download(
+    url: VO.NewspaperType["articles"][0]["url"]
+  ): Promise<null | string> {
+    try {
+      const result = await axios.get(url);
+
+      return result.data;
+    } catch (error) {
+      return null;
+    }
+  }
+}
