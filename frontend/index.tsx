14d2181e4915f7c9f35a6dad9afe0c63a4778618
Bartosz Gordon <bartosz.gordon@sealcode.org>
2022-01-06 22:53:40 +0100

    refactor: extract frontend app to another file

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -1,34 +1,5 @@
 import { h, render } from "preact";
-import { QueryClient, QueryClientProvider } from "react-query";
 
-import { AddArticleForm } from "./add-article-form";
-import { ArticleList } from "./article-list";
-import { NewspaperList } from "./newspaper-list";
-import { Stats } from "./stats";
-
-const queryClient = new QueryClient();
-
-function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <main
-        data-display="flex"
-        data-mx="auto"
-        data-mt="48"
-        style="max-width: 1296px"
-      >
-        <section data-grow="1" data-pr="48">
-          <AddArticleForm />
-          <Stats />
-        </section>
-
-        <div data-max-width="768" data-width="100%">
-          <ArticleList />
-          <NewspaperList />
-        </div>
-      </main>
-    </QueryClientProvider>
-  );
-}
+import { App } from "./app";
 
 render(<App />, document.querySelector("#root") as Element);
