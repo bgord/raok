b251b9bf6be745add04f87d5711a3f41acdf4437
Bartosz Gordon <bartosz.gordon@sealcode.org>
2022-01-06 19:16:00 +0100

    feature: add use list hook

---
+++ frontend/hooks.ts
@@ -0,0 +1,41 @@
+import { useState } from "preact/hooks";
+
+export function useList<T>(defaultItems: T[] = []): [
+  T[],
+  {
+    clear: VoidFunction;
+    add: (x: T | T[]) => void;
+    remove: (x: T) => void;
+    toggle: (x: T) => void;
+    isAdded: (x: T) => boolean;
+  }
+] {
+  const [items, setItems] = useState<T[]>(defaultItems);
+
+  function clear() {
+    setItems([]);
+  }
+
+  function add(payload: T | T[]) {
+    setItems((items) => {
+      if (Array.isArray(payload)) {
+        return [...items, ...payload];
+      }
+      return [...items, payload];
+    });
+  }
+
+  function remove(item: T) {
+    setItems((items) => items.filter((x) => x === item));
+  }
+
+  function isAdded(item: T) {
+    return items.some((x) => x === item);
+  }
+
+  function toggle(item: T) {
+    isAdded(item) ? remove(item) : add(item);
+  }
+
+  return [items, { clear, add, remove, toggle, isAdded }];
+}
