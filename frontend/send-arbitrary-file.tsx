c35320c8da4dac3cfc78c49bcb8a418b4edc828d
Bartosz Gordon <bartosz.gordon@sealcode.org>
2022-01-07 23:43:23 +0100

    step: add file selector form

--- frontend/send-arbitrary-file.tsx
+++ frontend/send-arbitrary-file.tsx
@@ -1,11 +1,78 @@
-import { h } from "preact";
+import { h, Fragment } from "preact";
 
 import { Header } from "./ui";
+import { useFile, UseFileState } from "./hooks";
+
+const style = {
+  width: "0.1px",
+  height: "0.1px",
+  opacity: "0",
+  overflow: "hidden",
+  position: "absolute",
+  zIndex: "-1",
+};
 
 export function SendArbitraryFile() {
+  const file = useFile();
+
   return (
-    <form data-mt="24" data-bg="gray-100" data-p="12">
+    <form
+      data-mt="24"
+      data-bg="gray-100"
+      data-p="12"
+      onSubmit={(event) => {
+        event.preventDefault();
+
+        if (file.state !== UseFileState.selected) return;
+
+        const form = new FormData();
+        form.append("file", file.data);
+      }}
+    >
       <Header data-mb="24">Send arbitrary file</Header>
+
+      <input
+        id="file"
+        name="file"
+        accept=".txt,.html,.epub,.mobi"
+        type="file"
+        onInput={file.actions.selectFile}
+        style={style}
+      />
+
+      <button
+        disabled={file.state === UseFileState.selected}
+        type="button"
+        class="c-button"
+        data-variant="secondary"
+      >
+        <label for="file">Choose a file</label>
+      </button>
+
+      {file.state === UseFileState.selected && (
+        <Fragment>
+          <button
+            type="submit"
+            class="c-button"
+            data-variant="primary"
+            data-ml="12"
+          >
+            Upload file
+          </button>
+
+          <button
+            type="button"
+            class="c-button"
+            data-variant="secondary"
+            data-ml="12"
+            onClick={file.actions.clearFile}
+          >
+            Clear
+          </button>
+        </Fragment>
+      )}
+
+      <div data-mt="24">{file.state}</div>
     </form>
   );
 }
