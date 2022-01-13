a7337ca2791b9eebaeefda9f206c47ed39b05ab0
Bartosz Gordon <bartosz.gordon@sealcode.org>
2022-01-13 23:45:42 +0100

    refactor: rename feedly articles crawler task

--- jobs.ts
+++ jobs.ts
@@ -9,51 +9,57 @@ import { Article } from "./aggregates/article";
 
 export const Scheduler = new ToadScheduler();
 
-const task = new AsyncTask("feedly articles crawler", async () => {
-  Reporter.info("Crawling Feedly articles...");
+const FeedlyArticlesCrawlerTask = new AsyncTask(
+  "feedly articles crawler",
+  async () => {
+    Reporter.info("Crawling Feedly articles...");
 
-  if (Env.SUPRESS_FEEDLY_CRAWLING === "yes") {
-    Reporter.info("Suppressing Feedly crawling due to feature flag");
-    return;
-  }
+    if (Env.SUPRESS_FEEDLY_CRAWLING === "yes") {
+      Reporter.info("Suppressing Feedly crawling due to feature flag");
+      return;
+    }
 
-  const articles = await Services.Feedly.getArticles();
-  Reporter.info(`Got ${articles.length} unread article(s).`);
+    const articles = await Services.Feedly.getArticles();
+    Reporter.info(`Got ${articles.length} unread article(s).`);
 
-  if (articles.length === 0) return;
+    if (articles.length === 0) return;
 
-  const insertedArticlesFeedlyIds: VO.FeedlyArticleType["id"][] = [];
+    const insertedArticlesFeedlyIds: VO.FeedlyArticleType["id"][] = [];
 
-  for (const article of articles) {
-    if (!article.canonicalUrl) continue;
+    for (const article of articles) {
+      if (!article.canonicalUrl) continue;
+
+      try {
+        await Article.add({
+          url: article.canonicalUrl,
+          source: VO.ArticleSourceEnum.feedly,
+        });
+        insertedArticlesFeedlyIds.push(article.id);
+
+        Reporter.success(
+          `Added article from Feedly [url=${article.canonicalUrl}]`
+        );
+      } catch (error) {
+        Reporter.error(`Article not added [url=${article.canonicalUrl}]`);
+      }
+    }
 
     try {
-      await Article.add({
-        url: article.canonicalUrl,
-        source: VO.ArticleSourceEnum.feedly,
-      });
-      insertedArticlesFeedlyIds.push(article.id);
+      if (insertedArticlesFeedlyIds.length === 0) return;
 
+      await Services.Feedly.markArticlesAsRead(insertedArticlesFeedlyIds);
       Reporter.success(
-        `Added article from Feedly [url=${article.canonicalUrl}]`
+        `Marked Feedly articles as read [ids=${insertedArticlesFeedlyIds}]`
       );
     } catch (error) {
-      Reporter.error(`Article not added [url=${article.canonicalUrl}]`);
+      Reporter.error("Failed to mark Feedly articles as read");
     }
   }
+);
 
-  try {
-    if (insertedArticlesFeedlyIds.length === 0) return;
+const FeedlyArticlesCrawlerJob = new SimpleIntervalJob(
+  { hours: 2, runImmediately: false },
+  FeedlyArticlesCrawlerTask
+);
 
-    await Services.Feedly.markArticlesAsRead(insertedArticlesFeedlyIds);
-    Reporter.success(
-      `Marked Feedly articles as read [ids=${insertedArticlesFeedlyIds}]`
-    );
-  } catch (error) {
-    Reporter.error("Failed to mark Feedly articles as read");
-  }
-});
-
-const job = new SimpleIntervalJob({ hours: 2, runImmediately: false }, task);
-
-Scheduler.addSimpleIntervalJob(job);
+Scheduler.addSimpleIntervalJob(FeedlyArticlesCrawlerJob);
