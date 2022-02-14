b52f4f4a01410671d7d76be90a7ab8d2bf0277c9
Bartosz Gordon <bartosz.gordon@sealcode.org>
2022-02-14 19:29:07 +0100

    feature: setup mobile navigation

--- frontend/navigation.tsx
+++ frontend/navigation.tsx
@@ -1,6 +1,8 @@
 import { Link } from "preact-router";
 import { h } from "preact";
-import { useWindowSize } from "./hooks";
+
+import * as Icons from "./icons";
+import { useWindowSize, useToggle } from "./hooks";
 
 export function Navigation() {
   const { width } = useWindowSize();
@@ -13,16 +15,7 @@ export function Navigation() {
 function NavigationDesktop() {
   return (
     <nav data-display="flex" data-p="12" data-bg="gray-800">
-      <NavigationLink
-        href="/dashboard"
-        data-fs="20"
-        data-ls="2"
-        data-color="gray-100"
-        data-fw="500"
-        data-mr="auto"
-      >
-        RAOK
-      </NavigationLink>
+      <NavigationLogo />
 
       <NavigationLink href="/archive/articles" data-mr="24">
         Articles
@@ -46,46 +39,55 @@ function NavigationDesktop() {
 }
 
 function NavigationMobile() {
+  const navigation = useToggle();
+
   return (
-    <nav data-display="flex" data-p="12" data-md-px="12" data-bg="gray-800">
-      <NavigationLink
-        href="/dashboard"
-        data-fs="20"
-        data-ls="2"
-        data-color="gray-100"
-        data-fw="500"
-        data-mr="auto"
+    <nav
+      data-display="flex"
+      data-cross="center"
+      data-py="6"
+      data-px="12"
+      data-bg="gray-800"
+    >
+      <NavigationLogo />
+
+      <button
+        type="button"
+        class="c-button"
+        data-variant="bare"
+        onClick={navigation.toggle}
       >
-        RAOK
-      </NavigationLink>
-
-      <NavigationLink href="/archive/articles" data-mr="24">
-        Articles
-      </NavigationLink>
-
-      <NavigationLink href="/archive/newspapers" data-mr="24">
-        Newspapers
-      </NavigationLink>
-
-      <NavigationLink href="/settings">Settings</NavigationLink>
-
-      <strong data-mx="36" data-color="white">
-        admin
-      </strong>
-
-      <NavigationLink data-mr="24" href="/logout">
-        Logout
-      </NavigationLink>
+        <Icons.HamburgerMenu />
+      </button>
     </nav>
   );
 }
 
 function NavigationShell() {
-  return <nav style={{ height: "48px" }} data-p="12" data-bg="gray-800" />;
+  return (
+    <nav data-p="12" data-bg="gray-800" style={{ height: "48px" }}>
+      <NavigationLogo />
+    </nav>
+  );
 }
 
 function NavigationLink(props: h.JSX.HTMLAttributes) {
   return (
     <Link class="c-link" data-color="white" data-variant="bare" {...props} />
+  );
+}
+
+function NavigationLogo() {
+  return (
+    <NavigationLink
+      href="/dashboard"
+      data-fs="20"
+      data-ls="2"
+      data-color="gray-100"
+      data-fw="500"
+      data-mr="auto"
+    >
+      RAOK
+    </NavigationLink>
   );
 }
