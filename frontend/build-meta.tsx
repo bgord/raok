99e4a447fc4332b25e2803132ae7b6ec06665d77
Bartosz Gordon <bartosz.gordon@sealcode.org>
2022-03-15 14:49:25 +0100

    fix: remove fixed position from build meta

--- frontend/build-meta.tsx
+++ frontend/build-meta.tsx
@@ -7,15 +7,7 @@ export type BuildMetaDataType = {
 
 export function BuildMeta(props: BuildMetaDataType) {
   return (
-    <div
-      data-position="fixed"
-      data-bottom="0"
-      data-left="0"
-      data-display="flex"
-      data-mr="6"
-      data-fs="12"
-      data-color="gray-400"
-    >
+    <div data-display="flex" data-mr="6" data-fs="12" data-color="gray-400">
       <div data-mx="12">{props.BUILD_VERSION}</div>
       <div>{new Date(props.BUILD_DATE).toLocaleString()}</div>
     </div>
