7151b1e7d2c41df8bde556e36c1ea6b48c6d726a
Bartosz Gordon <bartosz.gordon@sealcode.org>
2022-03-18 17:25:01 +0100

    refactor: improve filter types

--- services/filter.ts
+++ services/filter.ts
@@ -1,28 +1,47 @@
 import * as z from "zod";
 
-export class Filter<T extends z.ZodRawShape> {
+export type DefaultFilterSchemaType = z.ZodRawShape;
+export type FilterValuesType = Record<string, unknown>;
+export type FilterSchemaType<T extends DefaultFilterSchemaType> =
+  z.ZodObject<T>;
+
+export type FilterParseConfigType<T extends DefaultFilterSchemaType> = {
+  schema: FilterSchemaType<T>;
+  values: FilterValuesType;
+};
+
+export class Filter<T extends DefaultFilterSchemaType> {
   schema: z.ZodObject<T>;
 
-  constructor(schema: z.ZodObject<T>) {
+  constructor(schema: FilterSchemaType<T>) {
     this.schema = schema;
   }
 
-  parse(values: Record<string, unknown>) {
+  parse(values: FilterValuesType) {
     return Filter._parse({ schema: this.schema, values });
   }
 
-  static parse<T extends z.ZodRawShape>(config: {
-    schema: z.ZodObject<T>;
-    values: Record<string, unknown>;
-  }) {
+  default() {
+    return this.parse({});
+  }
+
+  static parse<T extends DefaultFilterSchemaType>(
+    config: FilterParseConfigType<T>
+  ) {
     return Filter._parse(config);
   }
 
-  private static _parse<T extends z.ZodRawShape>(config: {
-    schema: z.ZodObject<T>;
-    values: Record<string, unknown>;
-  }) {
+  static default<T extends DefaultFilterSchemaType>(
+    config: Omit<FilterParseConfigType<T>, "values">
+  ) {
+    return Filter._parse({ schema: config.schema, values: {} });
+  }
+
+  private static _parse<T extends DefaultFilterSchemaType>(
+    config: FilterParseConfigType<T>
+  ) {
     const result = config.schema.safeParse(config.values);
+
     return result.success ? result.data : undefined;
   }
 }
