70b8fcfb112018a73e2217ecacb830dd3a32a963
Bartosz Gordon <bartosz.gordon@sealcode.org>
2022-03-18 17:10:02 +0100

    refactor: make filter api more flexible

--- services/filter.ts
+++ services/filter.ts
@@ -1,12 +1,28 @@
 import * as z from "zod";
 
-export class Filter {
+export class Filter<T extends z.ZodRawShape> {
+  schema: z.ZodObject<T>;
+
+  constructor(schema: z.ZodObject<T>) {
+    this.schema = schema;
+  }
+
+  parse(values: Record<string, unknown>) {
+    return Filter._parse({ schema: this.schema, values });
+  }
+
   static parse<T extends z.ZodRawShape>(config: {
     schema: z.ZodObject<T>;
     values: Record<string, unknown>;
   }) {
-    const result = config.schema.safeParse(config.values);
+    return Filter._parse(config);
+  }
 
+  private static _parse<T extends z.ZodRawShape>(config: {
+    schema: z.ZodObject<T>;
+    values: Record<string, unknown>;
+  }) {
+    const result = config.schema.safeParse(config.values);
     return result.success ? result.data : undefined;
   }
 }
