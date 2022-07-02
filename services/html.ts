82b556894623ccb3fcdba153fa26e5bea79c89cf
Bartosz Gordon <bartosz.gordon@sealcode.org>
2022-07-02 07:23:32 +0200

    step: improve loading favicons

--- services/html.ts
+++ services/html.ts
@@ -22,18 +22,8 @@ export class Html {
           <link as="style" rel="stylesheet preload" href="/main.min.css" />
 
           <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
+            as="image"
+            rel="preload"
             type="image/png"
             sizes="16x16"
             href="/favicon-16x16.png"
