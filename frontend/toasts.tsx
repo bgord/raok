fb742986c345bd97f697d468500af389fbf0a8cf
Bartosz Gordon <bartosz.gordon@sealcode.org>
2022-08-04 16:38:21 +0200

    fix: multiple loading indicators in toast

--- frontend/toasts.tsx
+++ frontend/toasts.tsx
@@ -6,15 +6,6 @@ import * as api from "./api";
 import * as types from "./types";
 
 export function Toasts() {
-  const t = bg.useTranslations();
-
-  const queryClient = useQueryClient();
-  const undeleteArticle = useMutation(api.undeleteArticle, {
-    onSuccess: () => {
-      setTimeout(() => queryClient.invalidateQueries("articles"), 5000);
-    },
-  });
-
   const [_toasts] = bg.useToastsContext<types.ToastType>();
   const toasts = bg.useAnimaList(_toasts, { direction: "tail" });
 
@@ -29,42 +20,55 @@ export function Toasts() {
       style={{ maxWidth: "290px" }}
     >
       {toasts.items.map((toast) => (
-        <bg.Anima effect="opacity" {...toast.props}>
-          <li
-            key={toast.item.id}
-            aria-live="polite"
-            data-display="flex"
-            data-cross="center"
-            data-py="6"
-            data-px="12"
-            data-mt="12"
-            data-fs="14"
-            data-color="gray-700"
-            data-bg="gray-200"
-            data-br="2"
-            data-transform="upper-first"
-          >
-            {t(toast.item.message)}
-
-            {toast.item.message === "article.deleted" && (
-              <button
-                type="button"
-                class="c-button"
-                data-variant="bare"
-                data-ml="auto"
-                onClick={() => {
-                  if (!toast.item.articleId) return;
-                  undeleteArticle.mutate(toast.item.articleId);
-                }}
-              >
-                {undeleteArticle.isIdle && "undo"}
-                {undeleteArticle.isLoading && "..."}
-                {undeleteArticle.isSuccess && "✓"}
-              </button>
-            )}
-          </li>
+        <bg.Anima key={toast.item.id} effect="opacity" {...toast.props}>
+          <Toast {...toast} />
         </bg.Anima>
       ))}
     </bg.AnimaList>
+  );
+}
+
+function Toast(props: bg.UseAnimaListItemType<types.ToastType>) {
+  const queryClient = useQueryClient();
+
+  const undeleteArticle = useMutation(api.undeleteArticle, {
+    onSuccess: () => {
+      setTimeout(() => queryClient.invalidateQueries("articles"), 5000);
+    },
+  });
+
+  return (
+    <li
+      aria-live="polite"
+      data-display="flex"
+      data-cross="center"
+      data-py="6"
+      data-px="12"
+      data-mt="12"
+      data-fs="14"
+      data-color="gray-700"
+      data-bg="gray-200"
+      data-br="2"
+      data-transform="upper-first"
+    >
+      {props.item.message}
+
+      {props.item.message === "article.deleted" && (
+        <button
+          type="button"
+          class="c-button"
+          data-variant="bare"
+          data-ml="auto"
+          onClick={() => {
+            if (!props.item.articleId) return;
+            undeleteArticle.mutate(props.item.articleId);
+          }}
+        >
+          {undeleteArticle.isIdle && "undo"}
+          {undeleteArticle.isLoading && "..."}
+          {undeleteArticle.isSuccess && "✓"}
+        </button>
+      )}
+    </li>
   );
 }
