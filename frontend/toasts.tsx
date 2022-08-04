4b872f493ee5b9a26a087a575161affe8adb7772
Bartosz Gordon <bartosz.gordon@sealcode.org>
2022-08-04 16:53:47 +0200

    feature: add a title to article.deleted notification

--- frontend/toasts.tsx
+++ frontend/toasts.tsx
@@ -29,6 +29,7 @@ export function Toasts() {
 }
 
 function Toast(props: bg.UseAnimaListItemType<types.ToastType>) {
+  const t = bg.useTranslations();
   const queryClient = useQueryClient();
 
   const undeleteArticle = useMutation(api.undeleteArticle, {
@@ -40,8 +41,6 @@ function Toast(props: bg.UseAnimaListItemType<types.ToastType>) {
   return (
     <li
       aria-live="polite"
-      data-display="flex"
-      data-cross="center"
       data-py="6"
       data-px="12"
       data-mt="12"
@@ -49,26 +48,41 @@ function Toast(props: bg.UseAnimaListItemType<types.ToastType>) {
       data-color="gray-700"
       data-bg="gray-200"
       data-br="2"
-      data-transform="upper-first"
     >
-      {props.item.message}
+      <div
+        data-display="flex"
+        data-main="between"
+        data-cross="center"
+        data-width="100%"
+      >
+        {t(props.item.message)}
 
-      {props.item.message === "article.deleted" && (
-        <button
-          type="button"
-          class="c-button"
-          data-variant="bare"
-          data-ml="auto"
-          onClick={() => {
-            if (!props.item.articleId) return;
-            undeleteArticle.mutate(props.item.articleId);
-          }}
-        >
-          {undeleteArticle.isIdle && "undo"}
-          {undeleteArticle.isLoading && "..."}
-          {undeleteArticle.isSuccess && "✓"}
-        </button>
-      )}
+        {props.item.message === "article.deleted" && (
+          <button
+            type="button"
+            class="c-button"
+            data-variant="bare"
+            data-fs="16"
+            disabled={undeleteArticle.isSuccess}
+            onClick={() => {
+              if (!props.item.articleId) return;
+              undeleteArticle.mutate(props.item.articleId);
+            }}
+          >
+            {undeleteArticle.isIdle && "undo"}
+            {undeleteArticle.isLoading && "..."}
+            {undeleteArticle.isSuccess && "done"}
+          </button>
+        )}
+      </div>
+
+      <div
+        title={props.item.articleTitle ?? ""}
+        data-transform="truncate"
+        data-max-width="100%"
+      >
+        {props.item.articleTitle}
+      </div>
     </li>
   );
 }
