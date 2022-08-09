3ef38068f33b939634ad3bdf21d70f2b771f6289
Bartosz Gordon <bartosz.gordon@sealcode.org>
2022-08-09 15:43:28 +0200

    step: use dialog in delete all articles

--- frontend/delete-all-articles.tsx
+++ frontend/delete-all-articles.tsx
@@ -1,14 +1,16 @@
-import { h } from "preact";
+import { h, Fragment } from "preact";
 import { useMutation, useQueryClient } from "react-query";
-import { useToastTrigger, useTranslations } from "@bgord/frontend";
+import * as bg from "@bgord/frontend";
+import { Dialog } from "./dialog";
 
 import * as api from "./api";
 
 export function DeleteAllArticles(props: h.JSX.IntrinsicElements["button"]) {
-  const t = useTranslations();
+  const t = bg.useTranslations();
 
   const queryClient = useQueryClient();
-  const notify = useToastTrigger();
+  const notify = bg.useToastTrigger();
+  const dialog = bg.useToggle();
 
   const deleteAllArticles = useMutation(api.deleteAllArticles, {
     onSuccess() {
@@ -23,18 +25,22 @@ export function DeleteAllArticles(props: h.JSX.IntrinsicElements["button"]) {
   });
 
   return (
-    <button
-      type="button"
-      onClick={() => deleteAllArticles.mutate()}
-      disabled={deleteAllArticles.isLoading || deleteAllArticles.isSuccess}
-      class="c-button"
-      data-variant="bare"
-      {...props}
-    >
-      {deleteAllArticles.isIdle && t("articles.all.delete")}
-      {deleteAllArticles.isLoading && t("articles.all.deleting")}
-      {deleteAllArticles.isSuccess && t("articles.all.deleted")}
-      {deleteAllArticles.isError && t("articles.all.could_not_delete")}
-    </button>
+    <>
+      <button
+        type="button"
+        onClick={dialog.enable}
+        disabled={deleteAllArticles.isLoading || deleteAllArticles.isSuccess}
+        class="c-button"
+        data-variant="bare"
+        {...props}
+      >
+        {deleteAllArticles.isIdle && t("articles.all.delete")}
+        {deleteAllArticles.isLoading && t("articles.all.deleting")}
+        {deleteAllArticles.isSuccess && t("articles.all.deleted")}
+        {deleteAllArticles.isError && t("articles.all.could_not_delete")}
+      </button>
+
+      <Dialog {...dialog}>dialog</Dialog>
+    </>
   );
 }
