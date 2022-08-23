2cebf0f078029d8e30d0d7e7c78ccdcf5e1280df
Bartosz Gordon <bartosz.gordon@sealcode.org>
2022-08-23 22:36:51 +0200

    step: implement file download route

--- routes/download-file.ts
+++ routes/download-file.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import * as bg from "@bgord/node";
+import * as fs from "fs/promises";
+import path from "path";
 
 import * as Repos from "../repositories";
 
@@ -7,4 +9,18 @@ export async function DownloadFile(
   request: express.Request,
   response: express.Response,
   _next: express.NextFunction
-) {}
+) {
+  const fileId = bg.Schema.UUID.safeParse(request.params.fileId);
+  if (!fileId.success) return response.status(404).send("File doesn't exist");
+
+  const file = await Repos.FilesRepository.getSingle(fileId.data);
+  if (!file) return response.status(404).send("File doesn't exist");
+
+  try {
+    const content = await fs.readFile(path.resolve(file.path));
+
+    return response.download(content.toString());
+  } catch (error) {
+    return response.status(404).send("File doesn't exist");
+  }
+}
