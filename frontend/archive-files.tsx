5a1ac7db1d115b0551e9904ba06fc91b7b8b5b07
Bartosz Gordon <bartosz.gordon@sealcode.org>
2022-08-23 22:48:58 +0200

    refactor: reorganize archive files filters

--- frontend/archive-files.tsx
+++ frontend/archive-files.tsx
@@ -58,73 +58,80 @@ export function ArchiveFiles(props: RoutableProps) {
         <UI.Badge data-ml="12">{numberOfFiles}</UI.Badge>
       </div>
 
-      <div data-display="flex" data-cross="end" data-mt="12">
-        <div
-          data-display="flex"
-          data-direction="column"
-          data-mt="12"
-          data-mr="24"
-        >
-          <label class="c-label" htmlFor="sent-at">
-            Sent at
-          </label>
-          <UI.Select
-            id="sent-at"
-            name="sent-at"
-            value={sentAt.query}
-            onInput={sentAt.onChange}
+      <div
+        data-display="flex"
+        data-main="between"
+        data-cross="end"
+        data-gap="24"
+      >
+        <div data-display="flex" data-cross="end" data-mt="12">
+          <div
+            data-display="flex"
+            data-direction="column"
+            data-mt="12"
+            data-mr="24"
           >
-            {sentAt.options.map((option) => (
-              <option value={option}>{option}</option>
-            ))}
-          </UI.Select>
+            <label class="c-label" htmlFor="sent-at">
+              Sent at
+            </label>
+            <UI.Select
+              id="sent-at"
+              name="sent-at"
+              value={sentAt.query}
+              onInput={sentAt.onChange}
+            >
+              {sentAt.options.map((option) => (
+                <option value={option}>{option}</option>
+              ))}
+            </UI.Select>
+          </div>
+
+          <button
+            type="button"
+            class="c-button"
+            data-variant="bare"
+            onClick={sentAt.clear}
+          >
+            Reset filter
+          </button>
         </div>
 
-        <button
-          type="button"
-          class="c-button"
-          data-variant="bare"
-          onClick={sentAt.clear}
-        >
-          Reset filter
-        </button>
-      </div>
+        <div data-display="flex" data-max-width="100%">
+          <div data-position="relative">
+            <input
+              list="files"
+              onInput={search.onChange}
+              value={search.query}
+              class="c-input"
+              placeholder="Search for a file..."
+              style="padding-right: 36px"
+              data-width="100%"
+            />
+            <Search
+              height="34"
+              width="34"
+              data-position="absolute"
+              data-p="6"
+              style="top: 1px; right: 1px; background: white;"
+            />
+          </div>
 
-      <div data-display="flex" data-my="24" data-max-width="100%">
-        <div data-position="relative">
-          <input
-            list="files"
-            onInput={search.onChange}
-            value={search.query}
-            class="c-input"
-            placeholder="Search for a file..."
-            style="padding-right: 36px"
-            data-width="100%"
-          />
-          <Search
-            height="34"
-            width="34"
-            data-position="absolute"
-            data-p="6"
-            style="top: 1px; right: 1px; background: white;"
-          />
+          <button
+            type="button"
+            onClick={search.clear}
+            class="c-button"
+            data-variant="bare"
+            data-px="3"
+            data-ml="6"
+            data-mr="auto"
+          >
+            <Icons.Close />
+          </button>
         </div>
-
-        <button
-          type="button"
-          onClick={search.clear}
-          class="c-button"
-          data-variant="bare"
-          data-px="3"
-          data-ml="6"
-          data-mr="auto"
-        >
-          <Icons.Close />
-        </button>
       </div>
 
       {archiveFiles.isSuccess && archiveFiles.data.length === 0 && (
-        <div data-fs="14" data-color="gray-700">
+        <div data-fs="14" data-color="gray-700" data-mt="48">
           No archive files.
         </div>
       )}
@@ -140,6 +147,7 @@ export function ArchiveFiles(props: RoutableProps) {
         data-direction="column"
         data-gap="12"
         data-max-width="100%"
+        data-mt="48"
       >
         {files.map((file) => (
           <li
