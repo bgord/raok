d3705b3f85bddd147707d5eb9b77a2d91e2d0f08
Bartosz Gordon <bartosz.gordon@sealcode.org>
2022-09-15 17:32:11 +0200

    feature: add copy button to newspaper article

--- frontend/newspaper-article.tsx
+++ frontend/newspaper-article.tsx
@@ -1,14 +1,19 @@
 import { h } from "preact";
+import * as bg from "@bgord/frontend";
 
 import * as UI from "./ui";
 import * as types from "./types";
 import { FavouriteUnfavourite } from "./favourite-unfavourite";
 
 export function NewspaperArticle(props: types.NewspaperType["articles"][0]) {
+  const t = bg.useTranslations();
+  const notify = bg.useToastTrigger();
+
   return (
     <li
       data-display="flex"
       data-wrap="nowrap"
+      data-gap="6"
       data-max-width="768"
       data-cross="center"
       data-mb="12"
@@ -16,13 +21,20 @@ export function NewspaperArticle(props: types.NewspaperType["articles"][0]) {
     >
       {props.status === "processed" && <FavouriteUnfavourite {...props} />}
 
-      <UI.OutboundLink href={props.url} data-fs="14" data-mx="12">
+      <UI.OutboundLink href={props.url} data-fs="14">
         {props.url}
       </UI.OutboundLink>
 
-      <UI.Badge data-ml="auto" data-mr="6">
+      <UI.Badge data-md-display="none" data-ml="auto">
         {props.source}
       </UI.Badge>
+
+      <UI.CopyButton
+        options={{
+          text: props.url,
+          onSuccess: () => notify({ message: t("article.link.copied") }),
+        }}
+      />
     </li>
   );
 }
