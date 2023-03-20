5fedfeed511b28b8e0da8e11a922e8f5be4dcbb3
Bartosz Gordon <gordonbartosz@gmail.com>
2023-03-20 17:22:19 +0100

    step: add form validation

--- frontend/articles-search-form.tsx
+++ frontend/articles-search-form.tsx
@@ -1,10 +1,14 @@
 import { h } from "preact";
+
 import * as bg from "@bgord/frontend";
 import * as Icons from "iconoir-react";
 
 import * as UI from "./ui";
+import { ARTICLE_SEARCH_QUERY_MIN_LENGTH } from "../value-objects/article-search-query-min-length";
+import { ARTICLE_SEARCH_QUERY_MAX_LENGTH } from "../value-objects/article-search-query-max-length";
 
 export function ArticlesSearchForm() {
+  const t = bg.useTranslations();
   const search = bg.useClientSearch();
 
   return (
@@ -13,9 +17,10 @@ export function ArticlesSearchForm() {
       data-wrap="nowrap"
       data-max-width="100%"
       data-gap="6"
-      data-mb="48"
+      data-mb="36"
       onSubmit={(event) => {
         event.preventDefault();
+
         console.log({ query: search.query });
       }}
     >
@@ -23,12 +28,22 @@ export function ArticlesSearchForm() {
         <input
           list="articles"
           class="c-input"
-          placeholder="Search for an article..."
+          placeholder={t("articles.search.placeholder")}
           style="padding-right: 36px"
           data-width="100%"
-          pattern=".{3,512}"
-          onChange={search.onChange}
+          pattern={`.{${ARTICLE_SEARCH_QUERY_MIN_LENGTH},${ARTICLE_SEARCH_QUERY_MAX_LENGTH}}`}
+          onInvalid={(event) =>
+            // @ts-ignore
+            event.target.setCustomValidity(t("articles.search.validation"))
+          }
           value={search.query}
+          onChange={(event) => {
+            search.onChange(event);
+            if (isSearchQueryValid(search.query)) {
+              // @ts-ignore
+              event.target.setCustomValidity("");
+            }
+          }}
         />
         <Icons.Search
           height="34"
@@ -39,7 +54,14 @@ export function ArticlesSearchForm() {
         />
       </div>
 
-      <UI.ClearButton />
+      <UI.ClearButton onClick={search.clear} />
     </form>
+  );
+}
+
+function isSearchQueryValid(query: string) {
+  return (
+    query.length >= ARTICLE_SEARCH_QUERY_MIN_LENGTH &&
+    query.length <= ARTICLE_SEARCH_QUERY_MAX_LENGTH
   );
 }
