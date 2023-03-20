af61b1088f7a387e64837008bf8dd4327b349080
Bartosz Gordon <gordonbartosz@gmail.com>
2023-03-20 16:42:57 +0100

    fix: add missing load more translation

--- frontend/article-list.tsx
+++ frontend/article-list.tsx
@@ -178,7 +178,7 @@ export function ArticleList() {
             data-mx="auto"
             onClick={() => _articles.fetchNextPage()}
           >
-            Load more
+            {t("app.load_more")}
           </button>
         </div>
       )}
