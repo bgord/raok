2d2a24b08916bae275c8331dbf4cfbe89b7cefa0
Bartosz Gordon <gordonbartosz@gmail.com>
2023-03-20 16:38:47 +0100

    step: add min/max error handling

--- value-objects/article-search-query-min-length.ts
+++ value-objects/article-search-query-min-length.ts
@@ -1,1 +1,3 @@
 export const ARTICLE_SEARCH_QUERY_MIN_LENGTH = 3;
+export const ARTICLE_SEARCH_QUERY_MIN_LENGTH_ERROR_MESSAGE =
+  "article-search-query.min-length.error";
--- value-objects/article-search-query.ts
+++ value-objects/article-search-query.ts
@@ -1,12 +1,24 @@
 import { z } from "zod";
 
-import { ARTICLE_SEARCH_QUERY_MIN_LENGTH } from "./article-search-query-min-length";
-import { ARTICLE_SEARCH_QUERY_MAX_LENGTH } from "./article-search-query-max-length";
+import {
+  ARTICLE_SEARCH_QUERY_MIN_LENGTH,
+  ARTICLE_SEARCH_QUERY_MIN_LENGTH_ERROR_MESSAGE,
+} from "./article-search-query-min-length";
+import {
+  ARTICLE_SEARCH_QUERY_MAX_LENGTH,
+  ARTICLE_SEARCH_QUERY_MAX_LENGTH_ERROR_MESSAGE,
+} from "./article-search-query-max-length";
 
 export const ArticleSearchQuery = z
   .string()
-  .min(ARTICLE_SEARCH_QUERY_MIN_LENGTH)
-  .max(ARTICLE_SEARCH_QUERY_MAX_LENGTH)
+  .min(
+    ARTICLE_SEARCH_QUERY_MIN_LENGTH,
+    ARTICLE_SEARCH_QUERY_MIN_LENGTH_ERROR_MESSAGE
+  )
+  .max(
+    ARTICLE_SEARCH_QUERY_MAX_LENGTH,
+    ARTICLE_SEARCH_QUERY_MAX_LENGTH_ERROR_MESSAGE
+  )
   .brand<"article-search-query">();
 
 export type ArticleSearchQueryType = z.infer<typeof ArticleSearchQuery>;
