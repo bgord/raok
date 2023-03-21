b7437768dc04b8495b98183695c0992f845725df
Bartosz Gordon <gordonbartosz@gmail.com>
2023-03-21 22:46:07 +0100

    feature: add logger for event handler

--- error-handler.ts
+++ error-handler.ts
@@ -13,22 +13,39 @@ export class ErrorHandler {
     next
   ) => {
     if (error instanceof bg.Errors.InvalidCredentialsError) {
+      logger.error({
+        message: "Invalid credentials",
+        operation: "invalid_credentials_error",
+        requestId: request.requestId,
+      });
       return response.redirect("/");
     }
 
     if (error instanceof bg.Errors.AccessDeniedError) {
+      logger.error({
+        message: "Access denied",
+        operation: "access_denied_error",
+        requestId: request.requestId,
+      });
       return response.redirect("/");
     }
 
     if (error instanceof bg.Errors.FileNotFoundError) {
+      logger.error({
+        message: "File not found",
+        operation: "file_not_found_error",
+        requestId: request.requestId,
+      });
+
       return response.status(404).send("File not found");
     }
 
     if (error instanceof Policies.NonProcessedArticleUrlIsNotUniqueError) {
       logger.error({
         message: "Article URL is not unique",
-        operation: "error_handler",
+        operation: "non_processed_article_url_is_not_unique_error",
         requestId: request.requestId,
+        metadata: request.body,
       });
 
       return response
@@ -37,13 +54,23 @@ export class ErrorHandler {
     }
 
     if (error instanceof Policies.ShouldCrawlFeedlyError) {
+      logger.error({
+        message: "Feedly crawling stopped in the settings",
+        operation: "should_crawl_feedly_error",
+        requestId: request.requestId,
+      });
+
       return response
         .status(400)
         .send({ message: "dashboard.crawling.stopped", _known: true });
     }
 
-    /* eslint-disable no-console */
-    console.error(error);
+    logger.error({
+      message: "Unknown error",
+      operation: "unknown_error",
+      requestId: request.requestId,
+      metadata: { error },
+    });
 
     return next(error);
   };
