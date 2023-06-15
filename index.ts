bf1f3a9564111803d32b8978ac7c3dc82c5ef055
Bartosz Gordon <gordonbartosz@gmail.com>
2023-06-15 14:14:49 +0200

    refactor: cleanup index.ts

--- index.ts
+++ index.ts
@@ -5,122 +5,91 @@ import * as Routes from "./routes";
 import * as VO from "./value-objects";
 import * as infra from "./infra";
 
-const prerequisites = [
-  new bg.Prerequisite({
-    label: "pandoc",
-    binary: "pandoc",
-    strategy: bg.PrerequisiteStrategyEnum.exists,
-  }),
-
-  new bg.Prerequisite({
-    label: "calibre",
-    binary: "ebook-convert",
-    strategy: bg.PrerequisiteStrategyEnum.exists,
-  }),
-
-  new bg.Prerequisite({
-    label: "nodemailer",
-    strategy: bg.PrerequisiteStrategyEnum.mailer,
-    mailer: infra.Mailer,
-  }),
-];
-
-const Session = new bg.Session({
-  secret: infra.Env.COOKIE_SECRET,
-  store: bg.SessionFileStore.build({ ttl: bg.Time.Days(3).toSeconds() }),
-});
-
-const AuthShield = new bg.EnvUserAuthShield({
-  ADMIN_USERNAME: infra.Env.ADMIN_USERNAME,
-  ADMIN_PASSWORD: infra.Env.ADMIN_PASSWORD,
-});
-
-const BasicAuthShield = new bg.BasicAuthShield({
-  username: infra.Env.BASIC_AUTH_USERNAME,
-  password: infra.Env.BASIC_AUTH_PASSWORD,
-});
-
 const app = express();
 
 bg.addExpressEssentials(app);
 bg.Handlebars.applyTo(app);
 bg.I18n.applyTo(app);
-Session.applyTo(app);
-AuthShield.applyTo(app);
+infra.Session.applyTo(app);
+infra.AuthShield.applyTo(app);
 bg.HttpLogger.applyTo(app, infra.logger);
 
 app.get("/", bg.CsrfShield.attach, bg.Route(Routes.Home));
 
-app.get("/articles", AuthShield.verify, bg.Route(Routes.Articles));
-app.get("/articles/search", AuthShield.verify, bg.Route(Routes.ArticlesSearch));
+app.get("/articles", infra.AuthShield.verify, bg.Route(Routes.Articles));
+app.get(
+  "/articles/search",
+  infra.AuthShield.verify,
+  bg.Route(Routes.ArticlesSearch)
+);
 app.get(
   "/articles/archive",
-  AuthShield.verify,
+  infra.AuthShield.verify,
   bg.Route(Routes.ArchiveArticles)
 );
 
-app.post("/add-article", AuthShield.verify, bg.Route(Routes.AddArticle));
+app.post("/add-article", infra.AuthShield.verify, bg.Route(Routes.AddArticle));
 app.post(
   "/delete-article/:articleId",
-  AuthShield.verify,
+  infra.AuthShield.verify,
   bg.Route(Routes.DeleteArticle)
 );
 app.post(
   "/undelete-article/:articleId",
-  AuthShield.verify,
+  infra.AuthShield.verify,
   bg.Route(Routes.UndeleteArticle)
 );
 app.post(
   "/articles/all/delete",
-  AuthShield.verify,
+  infra.AuthShield.verify,
   bg.Route(Routes.DeleteAllArticles)
 );
 app.post(
   "/articles/old/delete",
-  AuthShield.verify,
+  infra.AuthShield.verify,
   bg.Route(Routes.DeleteOldArticles)
 );
 
-app.get("/newspapers", AuthShield.verify, bg.Route(Routes.Newspapers));
+app.get("/newspapers", infra.AuthShield.verify, bg.Route(Routes.Newspapers));
 app.get(
   "/newspapers/archive",
-  AuthShield.verify,
+  infra.AuthShield.verify,
   bg.Route(Routes.ArchiveNewspapers)
 );
 app.get(
   "/newspaper/:newspaperId",
-  AuthShield.verify,
+  infra.AuthShield.verify,
   bg.Route(Routes.SingleNewspaper)
 );
 app.get(
   "/newspaper/:newspaperId/read",
-  AuthShield.verify,
+  infra.AuthShield.verify,
   bg.Route(Routes.NewspaperRead)
 );
 app.post(
   "/create-newspaper",
-  AuthShield.verify,
+  infra.AuthShield.verify,
   bg.Route(Routes.CreateNewspaper)
 );
 app.post(
   "/archive-newspaper/:newspaperId",
-  AuthShield.verify,
+  infra.AuthShield.verify,
   bg.Route(Routes.ArchiveNewspaper)
 );
 app.post(
   "/cancel-newspaper/:newspaperId",
-  AuthShield.verify,
+  infra.AuthShield.verify,
   bg.Route(Routes.CancelNewspaper)
 );
 app.post(
   "/resend-newspaper/:newspaperId",
-  AuthShield.verify,
+  infra.AuthShield.verify,
   bg.Route(Routes.ResendNewspaper)
 );
 
 app.post(
   "/send-arbitrary-file",
-  AuthShield.verify,
+  infra.AuthShield.verify,
   ...bg.FileUploader.handle({
     autoClean: false,
     maxFilesSize: VO.MAX_UPLOADED_FILE_SIZE_BYTES,
@@ -129,74 +98,88 @@ app.post(
   }),
   bg.Route(Routes.SendArbitraryFile)
 );
-app.get("/files/archive", AuthShield.verify, bg.Route(Routes.ArchiveFiles));
+app.get(
+  "/files/archive",
+  infra.AuthShield.verify,
+  bg.Route(Routes.ArchiveFiles)
+);
 app.get(
   "/files/archive/:fileId/download",
-  AuthShield.verify,
+  infra.AuthShield.verify,
   bg.Route(Routes.DownloadFile)
 );
 
-app.get("/stats", AuthShield.verify, bg.Route(Routes.Stats));
-app.get("/settings", AuthShield.verify, bg.Route(Routes.Dashboard));
-app.get("/account/settings", AuthShield.verify, bg.Route(Routes.Settings));
+app.get("/stats", infra.AuthShield.verify, bg.Route(Routes.Stats));
+app.get("/settings", infra.AuthShield.verify, bg.Route(Routes.Dashboard));
+app.get(
+  "/account/settings",
+  infra.AuthShield.verify,
+  bg.Route(Routes.Settings)
+);
 
 app.post(
   "/disable-articles-to-review-notification",
-  AuthShield.verify,
+  infra.AuthShield.verify,
   bg.Route(Routes.DisableArticlesToReviewNotification)
 );
 app.post(
   "/enable-articles-to-review-notification",
-  AuthShield.verify,
+  infra.AuthShield.verify,
   bg.Route(Routes.EnableArticlesToReviewNotification)
 );
 app.post(
   "/set-articles-to-review-notification-hour",
-  AuthShield.verify,
+  infra.AuthShield.verify,
   bg.Route(Routes.SetArticlesToReviewNotificationHour)
 );
 
 app.post(
   "/stop-feedly-crawling",
-  AuthShield.verify,
+  infra.AuthShield.verify,
   bg.Route(Routes.StopFeedlyCrawling)
 );
 app.post(
   "/restore-feedly-crawling",
-  AuthShield.verify,
+  infra.AuthShield.verify,
   bg.Route(Routes.RestoreFeedlyCrawling)
 );
 
 app.get(
   "/archive/articles",
-  AuthShield.verify,
+  infra.AuthShield.verify,
   bg.Route(Routes.ArticlesArchive)
 );
 app.get(
   "/archive/newspapers",
-  AuthShield.verify,
+  infra.AuthShield.verify,
   bg.Route(Routes.NewspapersArchive)
 );
-app.get("/archive/files", AuthShield.verify, bg.Route(Routes.FilesArchive));
+app.get(
+  "/archive/files",
+  infra.AuthShield.verify,
+  bg.Route(Routes.FilesArchive)
+);
 
 app.post(
   "/schedule-feedly-articles-crawl",
   bg.RateLimitShield.build({ limitMs: bg.Time.Seconds(30).toMs() }),
-  AuthShield.verify,
+  infra.AuthShield.verify,
   bg.Route(Routes.ScheduleFeedlyArticlesCrawl)
 );
 
 app.post(
   "/login",
   bg.CsrfShield.verify,
-  AuthShield.attach,
+  infra.AuthShield.attach,
   (_request, response) => response.redirect("/dashboard")
 );
-app.get("/logout", AuthShield.detach, (_, response) => response.redirect("/"));
+app.get("/logout", infra.AuthShield.detach, (_, response) =>
+  response.redirect("/")
+);
 
 app.get(
   "/dashboard",
-  AuthShield.verify,
+  infra.AuthShield.verify,
   bg.CacheStaticFiles.handle(bg.CacheStaticFilesStrategy.never),
   bg.Route(Routes.Dashboard)
 );
@@ -205,9 +188,9 @@ app.get(
   "/healthcheck",
   bg.RateLimitShield.build({ limitMs: bg.Time.Minutes(1).toMs() }),
   bg.Timeout.build({ timeoutMs: bg.Time.Seconds(5).toMs() }),
-  BasicAuthShield.verify,
+  infra.BasicAuthShield.verify,
   bg.Healthcheck.build([
-    ...prerequisites,
+    ...infra.prerequisites,
     new bg.Prerequisite({
       label: "api",
       strategy: bg.PrerequisiteStrategyEnum.self,
@@ -233,7 +216,7 @@ app.use(Routes.ErrorHandler.handle);
   });
 
   const server = app.listen(infra.Env.PORT, async () => {
-    await bg.Prerequisites.check(prerequisites);
+    await bg.Prerequisites.check(infra.prerequisites);
 
     infra.logger.info({
       message: "Server has started",
--- infra/index.ts
+++ infra/index.ts
@@ -4,3 +4,42 @@ export * from "./html";
 export * from "./jobs";
 export * from "./logger";
 export * from "./mailer";
+
+import * as bg from "@bgord/node";
+import { Env } from "./env";
+import { Mailer } from "./mailer";
+
+export const prerequisites = [
+  new bg.Prerequisite({
+    label: "pandoc",
+    binary: "pandoc",
+    strategy: bg.PrerequisiteStrategyEnum.exists,
+  }),
+
+  new bg.Prerequisite({
+    label: "calibre",
+    binary: "ebook-convert",
+    strategy: bg.PrerequisiteStrategyEnum.exists,
+  }),
+
+  new bg.Prerequisite({
+    label: "nodemailer",
+    strategy: bg.PrerequisiteStrategyEnum.mailer,
+    mailer: Mailer,
+  }),
+];
+
+export const Session = new bg.Session({
+  secret: Env.COOKIE_SECRET,
+  store: bg.SessionFileStore.build({ ttl: bg.Time.Days(3).toSeconds() }),
+});
+
+export const AuthShield = new bg.EnvUserAuthShield({
+  ADMIN_USERNAME: Env.ADMIN_USERNAME,
+  ADMIN_PASSWORD: Env.ADMIN_PASSWORD,
+});
+
+export const BasicAuthShield = new bg.BasicAuthShield({
+  username: Env.BASIC_AUTH_USERNAME,
+  password: Env.BASIC_AUTH_PASSWORD,
+});
