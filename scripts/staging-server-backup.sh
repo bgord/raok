e8bd94c1d5fe3ea6b50ca48016bee335663400ac
Bartosz Gordon <gordonbartosz@gmail.com>
2023-10-08 16:30:44 +0200

    refactor: update backup script

--- scripts/staging-server-backup.sh
+++ scripts/staging-server-backup.sh
@@ -5,16 +5,18 @@ echo "Environment: staging"
 PROJECT_NAME=$1
 CURRENT_TIME=$(date +%F-%H-%M-%S)
 
-DATABASE_PATH="/var/www/$PROJECT_NAME/sqlite.db"
 BACKUPS_PATH="/var/backups/$PROJECT_NAME"
-BACKUP_PATH="$BACKUPS_PATH/$CURRENT_TIME.sqlite.backup"
 
-echo "Creating a backup of database: $DATABASE_PATH."
-sqlite3 $DATABASE_PATH ".backup $BACKUP_PATH"
-echo "Backup created to: $BACKUP_PATH."
+DATABASE_PATH="/var/www/$PROJECT_NAME/sqlite.db"
+DATABASE_BACKUP_PATH="$BACKUPS_PATH/$CURRENT_TIME.sqlite.backup"
 
-gzip $BACKUP_PATH
-echo "Backup compressed."
+echo "Creating a database backup: $DATABASE_PATH."
+sqlite3 $DATABASE_PATH ".backup $DATABASE_BACKUP_PATH"
+echo "Database backup created to: $DATABASE_BACKUP_PATH."
+
+tar -czf "$DATABASE_BACKUP_PATH.tar.gz" "$DATABASE_BACKUP_PATH"
+echo "Database backup compressed."
+rm "$DATABASE_BACKUP_PATH"
 
 find $BACKUPS_PATH/* -mtime +7 -exec rm {} \;
-echo "Files older than 7 days deleted"
+echo "Backups older than 7 days deleted"
