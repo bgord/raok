8d27729946b716724d79bfa1779ce56aec3354b1
Bartosz Gordon <gordonbartosz@gmail.com>
2023-11-14 16:24:29 +0100

    refactor: reading time calculator

--- modules/newspapers/services/estimated-reading-time-calculator.ts
+++ modules/newspapers/services/estimated-reading-time-calculator.ts
@@ -1,14 +1,15 @@
+import * as bg from "@bgord/node";
 import * as VO from "../value-objects";
 
 export class ReadingTimeCalculator {
-  static WORDS_PER_MINUTE = 225;
+  private static WORDS_PER_MINUTE = 225;
 
   static getMinutes(
     content: VO.ReadableArticleContentType
   ): VO.ReadableArticleReadingTimeType {
     const numberOfWords = content.split(" ").length;
 
-    const minutes = Math.ceil(
+    const minutes = new bg.RoundUp().round(
       numberOfWords / ReadingTimeCalculator.WORDS_PER_MINUTE
     );
 
