7f6be759f5c52061c590eb04caf66c9a7e050559
Bartosz Gordon <gordonbartosz@gmail.com>
2023-12-25 21:19:58 +0100

    feature: add delete token blacklist item button

--- frontend/settings-token-blacklist-item.tsx
+++ frontend/settings-token-blacklist-item.tsx
@@ -1,19 +1,37 @@
+import * as bg from "@bgord/frontend";
 import { h } from "preact";
+import { useMutation, useQueryClient } from "react-query";
 
+import * as api from "./api";
 import * as UI from "./ui";
 import * as types from "./types";
 
 export function SettingsTokenBlacklistItem(props: types.TokenBlacklistType) {
+  const queryClient = useQueryClient();
+  const notify = bg.useToastTrigger<types.ToastType>();
+
+  const deleteTokenBlacklist = useMutation(api.TokenBlacklist.delete, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(api.keys.tokenBlacklist);
+      notify({ message: "token_blacklist.deleted" });
+    },
+    onError: (error: bg.ServerError) => notify({ message: error.message }),
+  });
+
   return (
     <UI.Badge
       data-display="flex"
       data-wrap="nowrap"
       data-main="center"
       data-cross="center"
+      data-pl="12"
     >
       {props.token}
 
-      <UI.ClearButton />
+      <UI.ClearButton
+        disabled={deleteTokenBlacklist.isLoading}
+        onClick={() => deleteTokenBlacklist.mutate({ token: props.token })}
+      />
     </UI.Badge>
   );
 }
