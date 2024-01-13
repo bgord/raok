c873df5b890ce9b1f6bb02a2c551e7e0d4f74839
Bartosz Gordon <gordonbartosz@gmail.com>
2024-01-13 21:35:01 +0100

    step: add new stopwrods

--- modules/recommendations/value-objects/index.ts
+++ modules/recommendations/value-objects/index.ts
@@ -4,3 +4,4 @@ export * from "./token-rating";
 export * from "./blacklisted-token";
 export * from "./token-structure-error-key";
 export * from "./suggested-blacklisted-tokens-count";
+export * from "./stop-words";
