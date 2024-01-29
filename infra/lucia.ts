5eb6831dbe7f9dd6a52adb5fb87aaa6af5fb2977
Bartosz Gordon <gordonbartosz@gmail.com>
2024-01-29 13:32:14 +0100

    step: define DatabaseUserAttributes

--- infra/lucia.ts
+++ infra/lucia.ts
@@ -2,17 +2,27 @@ import { Lucia, TimeSpan } from "lucia";
 import { PrismaAdapter } from "@lucia-auth/adapter-prisma";
 
 import { Env } from "./env";
-import { db } from "./db";
+import { db, User } from "./db";
 
 const adapter = new PrismaAdapter(db.session, db.user);
 
 export const lucia = new Lucia(adapter, {
   sessionCookie: { attributes: { secure: Env.type === "production" } },
   sessionExpiresIn: new TimeSpan(4, "w"),
+  getUserAttributes: (attributes) => ({
+    email: attributes.email,
+    password: attributes.password,
+  }),
 });
 
 declare module "lucia" {
   interface Register {
     Lucia: typeof lucia;
+    DatabaseUserAttributes: DatabaseUserAttributes;
+  }
+
+  interface DatabaseUserAttributes {
+    email: User["email"];
+    password: User["password"];
   }
 }
