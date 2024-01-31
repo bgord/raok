8e0486575f7c081379a3317183c1ef9aef1664a7
Bartosz Gordon <gordonbartosz@gmail.com>
2024-01-31 17:37:00 +0100

    step: bump bgord/scripts

--- infra/lucia.ts
+++ infra/lucia.ts
@@ -4,11 +4,10 @@ import { PrismaAdapter } from "@lucia-auth/adapter-prisma";
 import * as Auth from "../auth";
 import { Env } from "./env";
 import { db, User, Session } from "./db";
-export type { User } from "./db";
 
 const adapter = new PrismaAdapter(db.session, db.user);
 
-export const lucia = new Lucia(adapter, {
+const lucia = new Lucia(adapter, {
   sessionCookie: { attributes: { secure: Env.type === "production" } },
   sessionExpiresIn: new TimeSpan(4, "w"),
   getUserAttributes: (attributes) => ({
