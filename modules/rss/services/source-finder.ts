6aa59b74a8484f5168bd5c287fd00a542c63a302
Bartosz Gordon <gordonbartosz@gmail.com>
2024-02-14 15:24:05 +0100

    feature: add autodiscovery source candidate finder

--- modules/rss/services/source-finder.ts
+++ modules/rss/services/source-finder.ts
@@ -30,7 +30,7 @@ export class SourceCandidateFinderStrategyYouTube {
       const dom = new JSDOM(response);
 
       const linkTag = dom.window.document.querySelector(
-        'link[rel="canonical"]'
+        'link[rel="canonical"]',
       );
 
       const url = linkTag?.getAttribute("href");
@@ -44,6 +44,42 @@ export class SourceCandidateFinderStrategyYouTube {
       return [
         `https://www.youtube.com/feeds/videos.xml?channel_id=${channelId}`,
       ];
+    } catch (error) {
+      return [value];
+    }
+  }
+}
+
+export class SourceCandidateFinderStrategyAutoDiscovery {
+  static strategy = "auto_discovery";
+
+  static isApplicable(value: VO.SourceUrlType): boolean {
+    try {
+      const url = new URL(value);
+      return url.origin !== "https://www.youtube.com";
+    } catch (error) {
+      return false;
+    }
+  }
+
+  static async get(value: VO.SourceUrlType): Promise<VO.SourceUrlType[]> {
+    try {
+      const result = await fetch(value, { method: "GET" });
+      const response = await result.text();
+
+      const dom = new JSDOM(response);
+
+      const linkTag = dom.window.document.querySelector(
+        'link[type="application/rss+xml"]',
+      );
+
+      const href = linkTag?.getAttribute("href");
+      if (!href) return [value];
+
+      const candidate = new URL(value);
+      candidate.pathname = href;
+
+      return [candidate.toString()];
     } catch (error) {
       return [value];
     }
@@ -79,7 +115,7 @@ export class SourceCandidateFinderStrategyDefault {
 export class SourceCandidatesFinder {
   static find(value: VO.SourceUrlType): SourceCandidateFinderStrategyType[] {
     const strategies: SourceCandidateFinderStrategyType[] = [
-      SourceCandidateFinderStrategyAutodiscovery,
+      SourceCandidateFinderStrategyAutoDiscovery,
       SourceCandidateFinderStrategyYouTube,
       SourceCandidateFinderStrategyDefault,
     ];
@@ -95,7 +131,7 @@ export class SourceFinder {
     const strategies = SourceCandidatesFinder.find(url);
 
     const candidates = await Promise.all(
-      strategies.map((strategy) => strategy.get(url))
+      strategies.map((strategy) => strategy.get(url)),
     );
 
     return new SourceFinder(candidates.flat());
@@ -103,7 +139,7 @@ export class SourceFinder {
 
   async find(): Promise<VO.SourceUrlType> {
     const options = this.candidates.map((sourceUrl) =>
-      Policies.SourceUrlResponds.perform({ sourceUrl }).then(() => sourceUrl)
+      Policies.SourceUrlResponds.perform({ sourceUrl }).then(() => sourceUrl),
     );
 
     return Promise.any(options).catch(() => {
