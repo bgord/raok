a6c7f1f0a4542df7395c658b9899edf40e53cd33
Bartosz Gordon <gordonbartosz@gmail.com>
2024-03-10 13:00:45 +0100

    step: add SourceQualityCalculator

---
+++ modules/rss/services/source-quality-calculator.ts
@@ -0,0 +1,19 @@
+import * as bg from "@bgord/node";
+import * as Newspapers from "../../newspapers";
+
+export class SourceQualityCalculator {
+  static calculate(
+    config: Record<Newspapers.VO.ArticleStatusEnum, number>,
+  ): number | null {
+    const total = Object.values(config).reduce((acc, curr) => acc + curr, 0);
+    if (total === 0) return null;
+
+    const considered = total - config.ready;
+    if (considered === 0) return null;
+
+    const interactedWith = config.in_progress + config.read + config.processed;
+    if (interactedWith === 0) return 0;
+
+    return new bg.RoundUp().round((interactedWith / considered) * 100);
+  }
+}
