8a5ea92374eadee035b57c777eaf81228ff80f83
Bartosz Gordon <gordonbartosz@gmail.com>
2024-05-13 21:32:31 +0200

    feature: add sidebar collapse

--- frontend/dashboard.tsx
+++ frontend/dashboard.tsx
@@ -1,6 +1,7 @@
 import { RoutableProps } from "preact-router";
 import { h } from "preact";
 import * as bg from "@bgord/frontend";
+import * as Icons from "iconoir-react";
 
 import * as hooks from "./hooks";
 import * as contexts from "./contexts";
@@ -23,11 +24,14 @@ export type InitialDashboardDataType = {
 export function Dashboard(_: RoutableProps) {
   hooks.useLeavingPrompt();
 
+  const t = bg.useTranslations();
   const scroll = bg.useScroll();
 
   const displayNonEssentialPanes = scroll.position.value <= 150;
 
   const [selectedArticleIds, actions] = bg.useList<types.ArticleType["id"]>();
+
+  const sidebar = bg.usePersistentToggle("sidebar-visible", true);
 
   return (
     <contexts.NewspaperCreatorProvider state={{ selectedArticleIds, actions }}>
@@ -39,9 +43,32 @@ export function Dashboard(_: RoutableProps) {
         data-md-mb="72"
         data-max-width="1296"
       >
+        <button
+          type="button"
+          class="c-button"
+          data-variant="with-icon"
+          data-position="absolute"
+          data-left="0"
+          data-z="2"
+          data-mt="6"
+          data-ml="6"
+          data-md-ml="3"
+          title={sidebar.on ? t("sidebar.collapse") : t("sidebar.expand")}
+          style={{
+            top: bg.Rhythm().times(4).px,
+            ...bg.Rhythm().times(2.5).square,
+          }}
+          onClick={sidebar.toggle}
+        >
+          {sidebar.on && <Icons.SidebarCollapse height="24" width="24" />}
+          {sidebar.off && <Icons.SidebarExpand height="24" width="24" />}
+        </button>
+
         <section
           data-width="100%"
           data-mt="6"
+          data-md-mt="24"
+          data-mb="24"
           data-pr="24"
           data-md-px="0"
           {...bg.Rhythm().times(62).style.maxWidth}
@@ -50,19 +77,21 @@ export function Dashboard(_: RoutableProps) {
           <NewspaperList />
         </section>
 
-        <section
-          data-grow="1"
-          data-mt="6"
-          data-px="24"
-          data-md-px="0"
-          data-width="100%"
-          data-md-max-width="unset"
-          {...bg.Rhythm().times(40).style.maxWidth}
-        >
-          <CreateNewspaper />
-          <SendArbitraryFile />
-          {displayNonEssentialPanes && <Stats />}
-        </section>
+        {sidebar.on && (
+          <section
+            data-grow="1"
+            data-mt="6"
+            data-px="24"
+            data-md-px="0"
+            data-width="100%"
+            data-md-max-width="unset"
+            {...bg.Rhythm().times(40).style.maxWidth}
+          >
+            <CreateNewspaper />
+            <SendArbitraryFile />
+            {displayNonEssentialPanes && <Stats />}
+          </section>
+        )}
       </main>
     </contexts.NewspaperCreatorProvider>
   );
