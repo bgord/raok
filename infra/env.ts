14f9f34422de21411381fe7f3f981df1ef8115f0
Bartosz Gordon <gordonbartosz@gmail.com>
2024-06-07 16:49:36 +0200

    feature: make env schema stricter

--- infra/env.ts
+++ infra/env.ts
@@ -1,22 +1,25 @@
 import * as bg from "@bgord/node";
 import { z } from "zod";
 
-const EnvironmentSchema = z.object({
-  PORT: bg.Schema.Port,
-  ADMIN_USERNAME: z.string().email(),
-  ADMIN_PASSWORD: bg.Schema.AdminPassword,
-  SMTP_HOST: bg.Schema.SmtpHost,
-  SMTP_PORT: bg.Schema.SmtpPort,
-  SMTP_USER: bg.Schema.SmtpUser,
-  SMTP_PASS: bg.Schema.SmtpPass,
-  EMAIL_FROM: bg.Schema.Email,
-  LOGS_LEVEL: bg.Schema.LogLevel,
-  BASIC_AUTH_USERNAME: bg.Schema.BasicAuthUsername,
-  BASIC_AUTH_PASSWORD: bg.Schema.BasicAuthPassword,
-  RSS_CRAWLING_ENABLED: bg.Schema.FeatureFlag,
-  HCAPTCHA_SECRET_KEY: bg.Schema.HCaptchaSecretKey,
-  HCAPTCHA_SITE_KEY: bg.Schema.HCaptchaSiteKey,
-});
+const EnvironmentSchema = z
+  .object({
+    PORT: bg.Schema.Port,
+    ADMIN_USERNAME: z.string().email(),
+    ADMIN_PASSWORD: bg.Schema.AdminPassword,
+    SMTP_HOST: bg.Schema.SmtpHost,
+    SMTP_PORT: bg.Schema.SmtpPort,
+    SMTP_USER: bg.Schema.SmtpUser,
+    SMTP_PASS: bg.Schema.SmtpPass,
+    EMAIL_FROM: bg.Schema.Email,
+    LOGS_LEVEL: bg.Schema.LogLevel,
+    BASIC_AUTH_USERNAME: bg.Schema.BasicAuthUsername,
+    BASIC_AUTH_PASSWORD: bg.Schema.BasicAuthPassword,
+    RSS_CRAWLING_ENABLED: bg.Schema.FeatureFlag,
+    HCAPTCHA_SECRET_KEY: bg.Schema.HCaptchaSecretKey,
+    HCAPTCHA_SITE_KEY: bg.Schema.HCaptchaSiteKey,
+    TZ: bg.Schema.TimezoneUTC,
+  })
+  .strict();
 type EnvironmentSchemaType = z.infer<typeof EnvironmentSchema>;
 
 export const Env = new bg.EnvironmentValidator<EnvironmentSchemaType>({
