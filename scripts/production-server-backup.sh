51758baf4ddc82817beba509e32c66bbf97a90ff
Bartosz Gordon <gordonbartosz@gmail.com>
2024-11-22 21:57:08 +0100

    step: cleanup

--- scripts/production-server-backup.sh
+++ scripts/production-server-backup.sh
@@ -11,9 +11,6 @@ BACKUPS_PATH="/var/backups/$PROJECT_NAME"
 DATABASE_PATH="/var/www/$PROJECT_NAME/sqlite.db"
 DATABASE_BACKUP_PATH="$BACKUPS_PATH/$CURRENT_TIME.sqlite.backup"
 
-NEWSPAPERS_PATH="/var/www/$PROJECT_NAME/newspapers"
-NEWSPAPERS_BACKUP_PATH="$BACKUPS_PATH/$CURRENT_TIME.newspapers.tar.gz"
-
 trap 'catch $? $LINENO' ERR
 
 catch() {
@@ -32,10 +29,6 @@ echo "Database backup created to: $DATABASE_BACKUP_PATH"
 tar -czf "$DATABASE_BACKUP_PATH.tar.gz" "$DATABASE_BACKUP_PATH"
 echo "Database backup compressed"
 rm "$DATABASE_BACKUP_PATH"
-
-echo "Creating a newspapers backup: $NEWSPAPERS_PATH"
-tar -czf "$NEWSPAPERS_BACKUP_PATH" "$NEWSPAPERS_PATH"
-echo "Newspapers backup created and compressed to: $NEWSPAPERS_BACKUP_PATH"
 
 find "$BACKUPS_PATH" -mtime +7 -exec rm {} \;
 echo "Backups older than 7 days deleted"
